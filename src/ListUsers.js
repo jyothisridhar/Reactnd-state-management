372d99a44c6638028eb003a97ac2d12549c4373d
Jyothi <jyotishridhar7@gmail.com>
2019-09-13 07:36:25 +0000

    first commit, exercise 1- passing data

---
+++ src/ListUsers.js
@@ -0,0 +1,14 @@
+import React, {Component}  from 'react';
+
+class ListUsers extends Component{
+  render(){
+    
+    return(
+      <ol>
+      	<li></li>
+      </ol>
+    )
+  }
+}
+
+export default ListUsers;
